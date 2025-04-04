860a5a52d26b9b5cf2874c91222daa89a92aadc2
ParkSinYoung <sean2010@naver.com>
2025-04-04 20:51:18 +0900

    Monster Attack

--- TDS_Project/Assets/Scripts/Monster2D.cs
+++ TDS_Project/Assets/Scripts/Monster2D.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Monster2D : MonoBehaviour
 {
     private Rigidbody2D rigidbody;
+    private Animator animator;
     public float speed = 2.0f;
+    public float attackInterval = 5.0f;
+    private bool isAttack = false;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -20,5 +24,39 @@ public class Monster2D : MonoBehaviour
     private void Move_2D()
     {
         rigidbody.velocity = new Vector2(-speed, rigidbody.velocity.y);
+    }
+
+    private IEnumerator IEAttack()
+    {
+        isAttack = true;
+        while (true)
+        {
+            animator.SetBool("IsAttacking", true);
+
+            yield return null;
+
+            if (!isAttack)
+            {
+                break;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Hero") && !isAttack)
+        {
+            StartCoroutine(IEAttack());
+        }
+    }
+    public void OnAttack()
+    {
+        Debug.Log("Attack");
+    }
+
+    public void OnAttackEnd()
+    {
+        animator.SetBool("IsAttacking", false);
+        Debug.Log("Attack End");
     }
 }
