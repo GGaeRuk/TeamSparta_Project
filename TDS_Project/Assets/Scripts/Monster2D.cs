422b4dd3601c0f732914c4a379faf659b739cf38
ParkSinYoung <sean2010@naver.com>
2025-04-05 16:07:16 +0900

    Monster Up

--- TDS_Project/Assets/Scripts/Monster2D.cs
+++ TDS_Project/Assets/Scripts/Monster2D.cs
@@ -10,6 +10,13 @@ public class Monster2D : MonoBehaviour
     public float attackInterval = 1.0f;
     private bool isAttack = false;
 
+    public float climbHeight = 1.2f;
+    private bool isOnTop = false;
+    private bool isClimbing = false;
+    private Vector2 climbStartPos;
+    private Vector2 climbEndPos;
+    public float climbSpeed = 2.0f;
+
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
@@ -18,7 +25,15 @@ public class Monster2D : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Move_2D();
+        if (!isOnTop)
+        {
+            Move_2D();
+        }
+
+        if (isClimbing)
+        {
+            ClimbOnTop();
+        }
     }
 
     private void Move_2D()
@@ -29,17 +44,13 @@ public class Monster2D : MonoBehaviour
     private IEnumerator IEAttack()
     {
         isAttack = true;
-        while (true)
+        while (isAttack)
         {
             animator.SetBool("IsAttacking", true);
 
             yield return null;
-
-            if (!isAttack)
-            {
-                break;
-            }
         }
+        animator.SetBool("IsAttacking", false);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -48,7 +59,42 @@ public class Monster2D : MonoBehaviour
         {
             StartCoroutine(IEAttack());
         }
+
+        if (collision.gameObject.CompareTag("Monster") && !isOnTop)
+        {
+            Monster2D otherMonster = collision.gameObject.GetComponent<Monster2D>();
+
+            if (otherMonster != null && transform.position.x > collision.transform.position.x)
+            {
+                climbStartPos = transform.position;
+                climbEndPos = new Vector2(transform.position.x, collision.transform.position.y + climbHeight);
+                isClimbing = true;
+            }
+        }
     }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Hero") && isAttack)
+        {
+            isAttack = false;
+            StopCoroutine(IEAttack());
+            animator.SetBool("IsAttacking", false);
+            Debug.Log("Attack End");
+        }
+    }
+
+    private void ClimbOnTop()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, climbEndPos, climbSpeed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, climbEndPos) < 0.1f)
+        {
+            isClimbing = false;
+            isOnTop = true;
+        }
+    }
+
     public void OnAttack()
     {
         Debug.Log("Attack");
